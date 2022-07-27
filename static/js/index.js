908ea4f2d40eb5b80a8983ad7d828395eb5e378d
William Renaldy <93593129+william-renaldy@users.noreply.github.com>
2022-07-27 18:08:17 +0530

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,37 @@
+function onlySpaces(str)
+{
+    return str.trim().length === 0;
+}
+
+$(document).ready(function()
+{
+    $("form").on("submit",function(event)
+    {
+        var date1 = document.getElementById("date1").value;
+        var date2 = document.getElementById("date2").value;
+        if (onlySpaces(date1) == false && onlySpaces(date2) == false)
+        {
+            document.getElementById("resultdiv").setAttribute("hidden",false);
+            document.getElementById("loading").removeAttribute("hidden");
+            $.ajax({
+                data:{
+                    date1:date1,
+                    date2:date2,
+                },
+                type: "POST",
+                url: "/submit",
+            }).done(function(data)
+            {
+                result = data;
+                alert();
+                document.getElementById("loading").setAttribute("hidden",false);
+                document.getElementById("result").innerHTML = result;
+                document.getElementById("resultdiv").removeAttribute("hidden");
+            });
+        }
+        else{
+            document.getElementById("resultdiv").setAttribute("hidden",false);
+        }
+    event.preventDefault();
+    });
+});
